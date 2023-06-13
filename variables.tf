632c9ba7d798238019b6cc4fa15a39be6d86f305
azizatenizova <126831827+azizatenizova@users.noreply.github.com>
2023-06-13 18:21:04 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,21 +3,27 @@ variable ami_id {
   default     = ""
   description = "description"
 }
+
 variable instance_type {
   type        = string
   default     = ""
   description = "description"
 }
+
+
 variable az1 {
   type        = string
   default     = ""
   description = "description"
 }
+
+
 variable key_pair {
   type        = string
   default     = ""
   description = "description"
 }
+
 variable region {
   type        = string
   default     = ""
